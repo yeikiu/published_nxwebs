918219bdfc9a1bcc00ec5d06b88d34ed03cff7b3
yeikiu <jqocon@gmail.com>
2026-04-30 15:19:07 +0100

    [CI:deploy] QA deployment from develop

--- fight-radar.com/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/published_nxwebs/fight-radar.com/sw.js', { scope: '/published_nxwebs/fight-radar.com/' })})}
